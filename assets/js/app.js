68991aa34a060b2a1f64e867ac8bd58ae0f51489
Kemal Akyigit <kemal.akyigit@hotmail.com>
2020-08-25 18:53:11 +0100

    logo&footer

--- assets/js/app.js
+++ assets/js/app.js
@@ -687,3 +687,10 @@ $('#submit-Bookingform').on('click', function(e) {
               $('#submit-Bookingform').html('Gönderiliyor..')  
           )                       
 })
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
