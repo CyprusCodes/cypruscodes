244cd57260c4f5ce89c47557fc4dff92baeb9912
Benjamin Mukadi <franckysourya@gmail.com>
2022-02-10 16:27:21 +0200

    fixed algorithm for language integration, and last check up

--- assets/js/ahmet.js
+++ assets/js/ahmet.js
@@ -2,73 +2,20 @@ $.i18n = function (options) {
   options = $.extend(
     {},
     {
-      lang: "tr",
+      lang: window.localStorage.getItem("language") || "tr",
       data: $.i18n,
-      sliceLang: false,
     },
     options
   );
 
-  var langStore = langStore || {},
-    lang =
-      options.lang.indexOf("-") < 0 && !options.sliceLang
-        ? options.lang
-        : options.lang.slice(0, 2);
-
-  if (typeof options.data !== "string") {
-    langStore = options.data[lang];
-  } else {
-    var urlParts = options.data.match(/(.*)[\/\\]([^\/\\]+)\.(\w+)$/);
-    $.ajax({
-      url: urlParts[1] + "/" + lang + "." + urlParts[3],
-      dataType: "json",
-      success: function (data) {
-        // console.log(data)
-        langStore = data;
-      },
-      error: function (error) {
-        console.log(error);
-        $.getJSON(
-          urlParts[1] + "/" + lang + "." + urlParts[3],
-          function (data) {
-            langStore = data;
-          }
-        );
-      },
-    });
-  }
-
-  var storeData = function (data) {
-    if (!data) return;
-    if (window.localStorage) {
-      localStorage.setItem("langStore", JSON.stringify(data));
-      langStore = data;
-    } else {
-      langStore = data;
-    }
-  };
-
-  if (window.localStorage) {
-    var localLangStore = localStorage.getItem("langStore");
-    storeData(localLangStore !== null ? JSON.parse(localLangStore) : langStore);
-  } else {
-    storeData(langStore);
-  }
-
-  this.getLang = function () {
-    return lang;
-  };
-  this.setLang = function (l) {
-    lang = l;
-    storeData(options.data[l]);
+  var langStore = options.data[options.lang];
+  this.setLang = function (language) {
+    window.localStorage.setItem("language", language);
+    langStore = options.data[language];
   };
 
   this.getItem = function (key) {
     return langStore[key];
-  };
-  this.setItem = function (key, value) {
-    options.data[lang][key] = value;
-    storeData(langStore);
   };
 
   return this;
@@ -163,16 +110,26 @@ var i18n = $.i18n();
 var changeLabels = function () {
   $("[data-translate]").each(function () {
     var forLabel = $(this).attr("data-translate");
-    this.innerHTML = i18n.getItem(forLabel);
+    const result = i18n.getItem(forLabel);
+    this.innerHTML = result;
   });
 };
-changeLabels();
+
+$(document).ready(function () {
+  changeLabels();
+  $("#dropdownMenuButton").text(
+    (window.localStorage.getItem("language") || "tr").toUpperCase()
+  );
+});
 
 $("#lang")
   .find("a")
   .click(function (e) {
     var lang = this.href.slice(-2);
     i18n.setLang(lang);
+    $("#dropdownMenuButton").text(
+      (window.localStorage.getItem("language") || "tr").toUpperCase()
+    );
 
     changeLabels();
     e.preventDefault();
